7951e1def925956f94d0d46e4e79195553e18905
André Gabriel Odon de Freitas <andreodon@gmail.com>
2024-08-21 17:47:17 -0300

    Conclusão da aula Anatomia das Classes -3

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -1,5 +1,13 @@
 public class MinhaClasse {
     public static void main(String[] args) {
-        System.out.print("Olá turma, sejam bem vindos!");
+        String primeiroNome = "André";
+        String segundoNome = "Freitas";
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
     }
 }
