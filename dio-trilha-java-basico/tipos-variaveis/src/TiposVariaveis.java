f525f89b82f4c6046eb88c340680af7087f169a0
André Gabriel Odon de Freitas <andreodon@gmail.com>
2024-08-26 16:28:52 -0300

    variaveis int e constantes

--- dio-trilha-java-basico/tipos-variaveis/src/TiposVariaveis.java
+++ dio-trilha-java-basico/tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,11 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numero = 10;
+        numero = 11;
+
+        final double NUMERO_PI = 3.14;
+
+        System.out.println(numero);
+        System.out.println(NUMERO_PI);
     }
 }
